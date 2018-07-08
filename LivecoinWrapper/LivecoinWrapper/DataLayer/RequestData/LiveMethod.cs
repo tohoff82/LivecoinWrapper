db3e856734b92ac67dd45d6e8e21c95fe97c2d9c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 16:15:57 +0300

    Реализован метод получения строки запроса последних торгов по текущей паре (данные за последнюю минуту или час)

--- LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethod.cs
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethod.cs
@@ -8,12 +8,24 @@ namespace LivecoinWrapper.DataLayer.RequestData
     {
         public static string GetTickers()
         {
-            return "/exchange/ticker";
+            return new StringBuilder("/exchange/ticker").ToString();
         }
 
         public static string GetTicker(string pair)
         {
-            return "/exchange/ticker?currencyPair=" + pair;
+            var sb = new StringBuilder("/exchange/ticker?");
+
+            return sb.AppendFormat("currencyPair={0}",pair).ToString();
+        }
+
+        public static string GetTradeline(string pair, string ordType = "false", string minOrHour = "false")
+        {
+            var sb = new StringBuilder("/exchange/last_trades?");
+            sb.AppendFormat("currencyPair={0}", pair);
+            if (ordType != "false") sb.AppendFormat("&type={0}", ordType);
+            if (minOrHour != "false") sb.AppendFormat("&minutesOrHour={0}", minOrHour);
+
+            return sb.ToString();
         }
     }
 }
