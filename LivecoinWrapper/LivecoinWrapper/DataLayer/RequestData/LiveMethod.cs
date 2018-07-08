1bf6f52b1527de7c0757d0a08f8255edd9c24a1c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 17:07:33 +0300

    Добавил меод генерирующий строку запроса для OllOrdeBok и мелкие правки

--- LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethod.cs
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethod.cs
@@ -6,26 +6,39 @@ namespace LivecoinWrapper.DataLayer.RequestData
 {
     public static class LiveMethod
     {
-        public static string GetTickers()
+        public static string GetTickersUri()
         {
             return new StringBuilder("/exchange/ticker").ToString();
         }
 
-        public static string GetTicker(string pair)
+        public static string GetTickerUri(string pair)
         {
             var sb = new StringBuilder("/exchange/ticker?");
 
             return sb.AppendFormat("currencyPair={0}",pair).ToString();
         }
+        //------------------------------------------------------------------------------------------//
 
-        public static string GetTradeline(string pair, string ordType = "false", string minOrHour = "false")
+        public static string GetLasttradeUri(string pair, string ordType = "false", string minOrHour = "false")
         {
             var sb = new StringBuilder("/exchange/last_trades?");
+
             sb.AppendFormat("currencyPair={0}", pair);
             if (ordType != "false") sb.AppendFormat("&type={0}", ordType);
             if (minOrHour != "false") sb.AppendFormat("&minutesOrHour={0}", minOrHour);
 
             return sb.ToString();
         }
+        //------------------------------------------------------------------------------------------//
+
+        public static string GetOllOrdebook(int depth = 10)
+        {
+            var sb = new StringBuilder("/exchange/all/order_book");
+
+            if (depth < 10) sb.AppendFormat("?depth={0}", depth.ToString());
+
+            return sb.ToString();
+        }
+
     }
 }
