3943440df60b555063ecad14da3be3484877bdd1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 18:47:56 +0300

    Объект ордеров по конкретной орговой паре

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/OrderBook.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    class OrderBook
+    {
+        [JsonProperty(PropertyName = "timestamp")]
+        public int Timestamp { get; set; }
+
+        [JsonProperty(PropertyName = "asks")]
+        public List<List<decimal>> Asks { get; set; }
+
+        [JsonProperty(PropertyName = "bids")]
+        public List<List<decimal>> Bids { get; set; }
+
+    }
+}
