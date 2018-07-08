654be579937e9cab5626f1eddda968c32f5c863e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 13:22:26 +0300

    Объект Ticker, получаемый при удачном ответе сервера на запрос информации по кокретной паре за 24 часа

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Ticker.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Ticker
+    {
+        [JsonProperty(PropertyName = "last")]
+        public decimal LastPrice { get; set; }
+
+        [JsonProperty(PropertyName = "high")]
+        public decimal HighPrice { get; set; }
+
+        [JsonProperty(PropertyName = "low")]
+        public decimal LowPrice { get; set; }
+
+        [JsonProperty(PropertyName = "volume")]
+        public decimal Volume { get; set; }
+
+        [JsonProperty(PropertyName = "vwap")]
+        public decimal VolumeWap { get; set; }
+
+        [JsonProperty(PropertyName = "max_bid")]
+        public decimal MaximumBid { get; set; }
+
+        [JsonProperty(PropertyName = "min_ask")]
+        public decimal MinimumAsk { get; set; }
+
+        [JsonProperty(PropertyName = "best_bid")]
+        public decimal BestBid { get; set; }
+
+        [JsonProperty(PropertyName = "best_ask")]
+        public decimal BestAsk { get; set; }
+    }
+}
