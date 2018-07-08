76c75c366e6020453a5705824a5a1646ec39f9c0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 14:56:44 +0300

    Методы получения тикера и масива тикеров

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public static class LiveMethod
+    {
+        public static string ExchengeTickers()
+        {
+            return "/exchange/ticker";
+        }
+
+        public static string ExchengeTicker(string pair)
+        {
+            return "/exchange/ticker?currencyPair=" + pair;
+        }
+    }
+}
