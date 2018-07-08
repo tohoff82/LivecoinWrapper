8512522de20c6aa024c72e24919422084a030166
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 12:55:43 +0300

    Статический класс - реализация валютных пар в сервисе.

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/MarketPair.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer
+{
+    public static class MarketPair
+    {
+        public static string BtcUsd { get { return "BTC/USD"; } }
+        public static string EmcBtc { get { return "EMC/BTC"; } }
+        public static string EmcUsd { get { return "EMC/BTC"; } }
+        public static string WgrBtc { get { return "WGR/BTC"; } }
+    }
+}
