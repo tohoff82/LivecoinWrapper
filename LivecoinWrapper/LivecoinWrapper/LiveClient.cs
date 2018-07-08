066e0b604e4e8a63edf5bfc5a3133f89a1c6d4b0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 14:58:43 +0300

    Клиент умеет доставать данные валютной пары по паблик меоду ExchangeTicker

--- LivecoinWrapper/LivecoinWrapper/LiveClient.cs
+++ LivecoinWrapper/LivecoinWrapper/LiveClient.cs
@@ -1,8 +1,60 @@
-﻿using System;
+﻿using LivecoinWrapper.DataLayer;
+using LivecoinWrapper.DataLayer.ReciveData;
+using LivecoinWrapper.DataLayer.RequestData;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace LivecoinWrapper
 {
     public class LiveClient
     {
+        private HttpClient httpClient;
+
+        private string publicKey;
+        private string secretKey;
+
+        private const string baseAddress = "https://api.livecoin.net";
+
+        public LiveClient()
+        {
+            httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        }
+
+        public LiveClient(string pubKey, string secKey)
+        {
+            publicKey = pubKey;
+            secretKey = secKey;
+
+            httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+        }
+
+        protected async Task<T> GetJsonAsync<T>(string requestUri)
+        {
+            var response = await httpClient.GetAsync(requestUri).ConfigureAwait(false);
+
+            response.EnsureSuccessStatusCode();         // throw if web request failed
+
+            var json = await response.Content.ReadAsStringAsync();
+
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(json));
+        }
+
+        #region Public Methods
+        public async Task<List<Ticker>> GetTickerAsync()
+        {
+            return await GetJsonAsync<List<Ticker>>(LiveMethod.ExchengeTickers());
+        }
+
+        public async Task<Ticker> GetTickerAsync(string marketPair)
+        {
+            return await GetJsonAsync<Ticker>(LiveMethod.ExchengeTicker(marketPair));
+        }
+
+        #endregion
+
+
     }
 }
