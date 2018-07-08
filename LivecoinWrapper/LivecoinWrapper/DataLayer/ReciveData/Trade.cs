174c20be874b65b4f74f33023a2cadc2fef8a51b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 15:28:54 +0300

    Объект оргов (сделка) на бирже

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Trade.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Trade
+    {
+        [JsonProperty(PropertyName = "time")]
+        public int Time { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty(PropertyName = "quantity")]
+        public decimal Quantity { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string OrderType { get; set; }
+    }
+}
