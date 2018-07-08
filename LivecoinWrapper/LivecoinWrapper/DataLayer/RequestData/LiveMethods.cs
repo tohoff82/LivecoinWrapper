6482edebcc855796e5a29aafd9ac286676b41191
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-08 13:33:27 +0300

    Данный статический класс будет возвращать сегмент строки запроса, овечающий за вызов определенного метода биржи

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/LiveMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public static class LiveMethods
+    {
+        public static string ExchangeTicker { get { return "/exchange/ticker"; } }
+    }
+}
