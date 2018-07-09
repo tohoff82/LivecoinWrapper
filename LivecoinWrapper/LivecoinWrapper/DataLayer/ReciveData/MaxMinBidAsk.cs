c5825c41f0b2c3d98bf92a8647c161e5ff394cfd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-09 13:45:04 +0300

    Объект содержащий минимальный аск и макс бид

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/MaxMinBidAsk.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class MaxMinBidAsk
+    {
+        [JsonProperty(PropertyName = "currencyPairs")]
+        List<CurrencyPairs> Currencies { get; set; }
+    }
+
+    public class CurrencyPairs
+    {
+        [JsonProperty(PropertyName = "symbol")]
+        public string Symbol { get; set; }
+
+        [JsonProperty(PropertyName = "maxBid")]
+        private readonly string maxBid;
+
+        [JsonProperty(PropertyName = "minAsk")]
+        private readonly string minAsk;
+
+
+        //Сделал эти костыли по причине того, что иногда в ответе цена представлена
+        //строкой в таком виде -->  1.4E-4 по другому как распарсить пока не придумал
+
+        public decimal MaxBid
+        {
+            get
+            {
+                return (decimal)Convert.ToDouble(maxBid.Replace(".", ","));
+            }
+        }
+
+        public decimal MinAsk
+        {
+            get
+            {
+                return (decimal)Convert.ToDouble(minAsk.Replace(".", ","));
+            }
+        }
+    }
+}
