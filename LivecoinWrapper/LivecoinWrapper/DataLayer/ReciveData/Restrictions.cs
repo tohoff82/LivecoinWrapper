f70b95c78278dd7323687ad6d9cbe2339a786c06
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-07-09 15:47:55 +0300

    Объект ограничений для каждой торговой пары

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Restrictions.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Restrictions
+    {
+        [JsonProperty(PropertyName = "success")]
+        public bool Success { get; set; }
+
+        [JsonProperty(PropertyName = "minBtcVolume")]
+        private readonly string minBtcVolume;
+
+        [JsonProperty(PropertyName = "restrictions")]
+        public List<Restriction> RestrictionList { get; set; }
+
+
+        //Сделал эти костыли по причине того, что иногда в ответе цена представлена
+        //строкой в таком виде -->  1.4E-4 по другому как распарсить пока не придумал
+
+        public decimal MinBtcVolume
+        {
+            get
+            {
+                return (decimal)Convert.ToDouble(minBtcVolume.Replace(".", ","));
+            }
+        }
+    }
+
+    public class Restriction
+    {
+        [JsonProperty(PropertyName = "currencyPair")]
+        public string CurrencyPair { get; set; }
+
+        [JsonProperty(PropertyName = "priceScale")]
+        public byte PriceScale { get; set; }
+
+        [JsonProperty(PropertyName = "inLimitQuantity")]
+        private readonly string inLimitQuantity;
+
+
+
+        public decimal InLimitQuantity
+        {
+            get
+            {
+                return (decimal)Convert.ToDouble(inLimitQuantity.Replace(".", ","));
+            }
+        }
+    }
+}
