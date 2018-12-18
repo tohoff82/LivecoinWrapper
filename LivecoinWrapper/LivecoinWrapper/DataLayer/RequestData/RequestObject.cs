273ee6e1229652afa2ad45ab8dfad6a29c105b66
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 05:53:59 +0200

    Create RequestObject.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/RequestObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using LivecoinWrapper.Helper;
+using static LivecoinWrapper.Helper.Enums;
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public abstract class RequestObject
+    {
+        protected const string urlSegmentExchange = "/exchange/";
+        protected const string urlSegmentPayment  = "/payment/";
+        protected const string urlSegmentInfo     = "/info/";
+
+        internal Dictionary<string, string> arguments;
+
+        internal string Url { get; private set; }
+
+        protected void GenerateRequest(RequestType type, string method)
+        {
+            switch (type)
+            {
+                case exchange: Url = new StringBuilder(urlSegmentExchange).AppendFormat("{0}?{1}", method, arguments.ToKeyValueString()).ToString();
+                    break;
+                case exchangeAuth:
+                    break;
+                case payment:
+                    break;
+                case info:
+                    break;
+                //default:
+                //    break;
+            }
+        }
+
+
+    }
+}
