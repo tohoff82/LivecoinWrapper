092d6368e3ebeb5b4966f8a069e5b0b89b5f67a0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 06:10:33 +0200

    Create OrderType.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/OrderType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.DataLayer
+{
+    public static class OrderType
+    {
+        public const string defoult = "false";
+        public const string buy     = "buy";
+        public const string sell    = "sell";
+    }
+}
