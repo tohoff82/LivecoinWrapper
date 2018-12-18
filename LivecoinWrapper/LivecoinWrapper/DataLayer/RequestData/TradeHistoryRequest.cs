6fc7b30a341752cbc6158f4046cfae0cb7afb445
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 17:22:58 +0200

    Create TradeHistoryRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/TradeHistoryRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.DataLayer.OrderType;
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class TradeHistoryRequest : RequestObject
+    {
+        public TradeHistoryRequest(string pairId, bool minOrHour, string orderType) : base()
+        {
+            arguments = new Dictionary<string, string>
+            {
+                ["currencyPair"]  = pairId,
+                ["minutesOrHour"] = minOrHour.ToString()
+            };
+
+            if (orderType != defoult) arguments.Add("type", orderType);
+
+            GenerateRequest(exchange, "last_trades");
+        }
+    }
+}
