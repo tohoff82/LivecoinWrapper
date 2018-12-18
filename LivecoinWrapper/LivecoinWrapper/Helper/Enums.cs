e816f7775bf6e8a9fc5b2e14288081dea0a49ed0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 06:02:06 +0200

    Create Enums.cs

---
+++ LivecoinWrapper/LivecoinWrapper/Helper/Enums.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper.Helper
+{
+    public class Enums
+    {
+        public enum RequestType { exchange, exchangeAuth, payment, info}
+    }
+}
