823b8c651bf56c5428181dd4cf91c72a2403bc15
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 18:30:28 +0200

    Update MarketPair.cs

--- LivecoinWrapper/LivecoinWrapper/DataLayer/MarketPair.cs
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/MarketPair.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LivecoinWrapper.DataLayer
+﻿namespace LivecoinWrapper.DataLayer
 {
     public static class MarketPair
     {
