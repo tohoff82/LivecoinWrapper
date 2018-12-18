bfbbece58d4fe3ad288c5318f8728d603c095105
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 05:54:13 +0200

    Create LiveClientPublic.cs

---
+++ LivecoinWrapper/LivecoinWrapper/LiveClientPublic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LivecoinWrapper.DataLayer.RequestData;
+
+namespace LivecoinWrapper
+{
+    public class LiveClientPublic : LiveClient
+    {
+        public LiveClientPublic() : base() { }
+
+        /// <summary>
+        /// Get information for the last 24 hours on a specific currency pair.
+        /// </summary>
+        /// <typeparam name="T"> Ticker or List(Ticker) </typeparam>
+        /// <param name="pairId">allPair or concret pair </param>
+        /// <returns>Ticker or List(Ticker)</returns>
+        public async Task<T> GetTickerAsync<T>(string pairId) =>
+                await HttpGetAsync<T>(new TickerRequest(pairId));
+    }
+}
