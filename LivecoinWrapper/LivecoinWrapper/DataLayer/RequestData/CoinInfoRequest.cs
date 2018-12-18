88c70e6db243e2dc7534256fc934ddcc143aad57
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 20:55:34 +0200

    Create CoinInfoRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/CoinInfoRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class CoinInfoRequest : RequestObject
+    {
+        public CoinInfoRequest() : base()
+        {
+            GenerateRequest(info, "coinInfo");
+        }
+    }
+}
