127635b3b1f519577cfc10b1eb8a088501c46fe4
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 05:57:05 +0200

    Update LiveClient.cs

--- LivecoinWrapper/LivecoinWrapper/LiveClient.cs
+++ LivecoinWrapper/LivecoinWrapper/LiveClient.cs
@@ -1,5 +1,4 @@
-﻿using LivecoinWrapper.DataLayer;
-using LivecoinWrapper.DataLayer.ReciveData;
+﻿using LivecoinWrapper.DataLayer.ReciveData;
 using LivecoinWrapper.DataLayer.RequestData;
 using Newtonsoft.Json;
 using System;
@@ -9,9 +8,9 @@ using System.Threading.Tasks;
 
 namespace LivecoinWrapper
 {
-    public class LiveClient
+    public abstract class LiveClient : IDisposable
     {
-        private HttpClient httpClient;
+        protected HttpClient httpClient;
 
         private const string baseAddress = "https://api.livecoin.net";
 
@@ -20,64 +19,73 @@ namespace LivecoinWrapper
             httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
         }
 
-        protected async Task<T> JsonGetAsync<T>(string requestUri)
+        public LiveClient(string apiKey, string apiSec)
         {
-            var response = await httpClient.GetAsync(requestUri).ConfigureAwait(false);
+            // todo
+        }
+
+        protected async Task<T> HttpGetAsync<T>(RequestObject requestObj)
+        {
+            var response = await httpClient.GetAsync(requestObj.Url).ConfigureAwait(false);
 
             response.EnsureSuccessStatusCode();         // throw if web request failed
 
-            var json = await response.Content.ReadAsStringAsync();
+            string json = await response.Content.ReadAsStringAsync();
 
-            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(json));
+            return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
         }
+
+        //protected async Task<T> HttpPostAsync<T>(RequestObject requestObj)
+        //{
+        //    //todo
+
+        //    return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(json));
+        //}
 
 
         #region Public Methods
 
-        public async Task<List<Ticker>> GetTickerAsync()
-        {
-            return await JsonGetAsync<List<Ticker>>(GetMethod.TickerUri());
-        }
-
-        public async Task<Ticker> GetTickerAsync(string marketPair)
-        {
-            return await JsonGetAsync<Ticker>(GetMethod.TickerUri(marketPair));
-        }
+        //public async Task<Ticker> GetTickerAsync(string marketPair)
+        //{
+        //    return await HttpGetAsync<Ticker>(GetMethod.TickerUri(marketPair));
+        //}
 
 
-        public async Task<List<Trade>> GetLastTradesAsync(string marketPair, string ordType = "false", string minOrHour = "false")
-        {
-            // ordType = "BUY" or "SELL", "false"(по умолчанию)
-            // minOrHour = "true" - данные за последнюю минуту, "false"(по умолчанию) - данные за час
-            return await JsonGetAsync<List<Trade>>(GetMethod.LasttradeUri(marketPair, ordType, minOrHour));
-        }
+        //public async Task<List<PublicTrade>> GetLastTradesAsync(string marketPair, string ordType = "false", string minOrHour = "false")
+        //{
+        //    // ordType = "BUY" or "SELL", "false"(по умолчанию)
+        //    // minOrHour = "true" - данные за последнюю минуту, "false"(по умолчанию) - данные за час
+        //    return await HttpGetAsync<List<PublicTrade>>(GetMethod.LasttradeUri(marketPair, ordType, minOrHour));
+        //}
 
 
-        public async Task<object> GetAllOrderBookAsync()
-        {
-            return await JsonGetAsync<object>(GetMethod.AllOrdebookUri());
-        }
+        //public async Task<object> GetAllOrderBookAsync()
+        //{
+        //    return await HttpGetAsync<object>(GetMethod.AllOrdebookUri());
+        //}
 
-        public async Task<OrderBook> GetOrderBookAsync(string marketPair, int depth = 100)
-        {
-            return await JsonGetAsync<OrderBook>(GetMethod.OrderbookUri(marketPair, depth));
-        }
+        //public async Task<OrderBook> GetOrderBookAsync(string marketPair, int depth = 100)
+        //{
+        //    return await HttpGetAsync<OrderBook>(GetMethod.OrderbookUri(marketPair, depth));
+        //}
 
 
-        public async Task<MaxMinBidAsk> GetMaxMinBidAskAsync(string marketPair = "allPairs")
-        {
-            return await JsonGetAsync<MaxMinBidAsk>(GetMethod.MaxMinBidAskUri(marketPair));
-        }
+        //public async Task<MaxMinBidAsk> GetMaxMinBidAskAsync(string marketPair = "allPairs")
+        //{
+        //    return await HttpGetAsync<MaxMinBidAsk>(GetMethod.MaxMinBidAskUri(marketPair));
+        //}
 
-        public async Task<Restrictions> GetRestrictionsAsync()
-        {
-            return await JsonGetAsync<Restrictions>(GetMethod.RestrictionsUri());
-        }
+        //public async Task<Restrictions> GetRestrictionsAsync()
+        //{
+        //    return await HttpGetAsync<Restrictions>(GetMethod.RestrictionsUri());
+        //}
 
-        public async Task<Coins> GetCoinInfoAsync()
-        {
-            return await JsonGetAsync<Coins>(GetMethod.CoinInfoUri());
-        }
+        //public async Task<Coins> GetCoinInfoAsync()
+        //{
+        //    return await HttpGetAsync<Coins>(GetMethod.CoinInfoUri());
+        //}
+
+        public void Dispose() => httpClient.Dispose();
 
         #endregion
     }
