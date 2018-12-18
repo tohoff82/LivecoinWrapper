80d51e53332f00114f5b71b98456552ec2116547
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 20:03:24 +0200

    Create RestrictionRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/RestrictionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class RestrictionRequest : RequestObject
+    {
+        public RestrictionRequest() : base()
+        {
+            arguments = new Dictionary<string, string>();
+
+            GenerateRequest(exchange, "restrictions");
+        }
+    }
+}
