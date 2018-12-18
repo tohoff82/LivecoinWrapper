a3d9f25451c9229bc51bc32025d0f10e6a86c10a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 05:34:22 +0200

    Create TickerRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/TickerRequest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static LivecoinWrapper.Helper.Enums.RequestType;
+using static LivecoinWrapper.DataLayer.MarketPair;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class TickerRequest : RequestObject
+    {
+        public TickerRequest(string pairId): base()
+        {
+            if (pairId != allPair)
+                arguments = new Dictionary<string, string>
+                {
+                    ["currencyPair"] = pairId
+                };
+            else arguments = new Dictionary<string, string>();
+
+            GenerateRequest(exchange, "ticker");
+        }
+    }
+}
