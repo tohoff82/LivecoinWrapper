956cd611979394064f90738ed113895599166b5b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 20:16:38 +0200

    Update Enums.cs

--- LivecoinWrapper/LivecoinWrapper/Helper/Enums.cs
+++ LivecoinWrapper/LivecoinWrapper/Helper/Enums.cs
@@ -7,5 +7,17 @@ namespace LivecoinWrapper.Helper
     public class Enums
     {
         public enum RequestType { exchange, exchangeAuth, payment, info}
+
+        public enum WalletStatus
+        {
+            normal,             //Wallet works fine
+            delayed,            //Wallet is delayed (no new unit 1-2 hours)
+            blocked,            //Wallet is not synchronized (there is no new unit for at least 2 hours)
+            blocked_long,       //Last block received more than 24 hours ago
+            down,               //Wallet is temporarily off
+            delisted,           //The coin will be removed from the exchange, withdraw your funds.
+            closed_cashin,      //Only output allowed
+            closed_cashout      //Only input is allowed
+        }
     }
 }
