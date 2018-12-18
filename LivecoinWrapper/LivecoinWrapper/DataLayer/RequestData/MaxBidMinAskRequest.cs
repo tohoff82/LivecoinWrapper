84e2099f20529f943f4c866138f17d9bfe5fda07
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 19:24:44 +0200

    Create MaxBidMinAskRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/MaxBidMinAskRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class MaxBidMinAskRequest : RequestObject
+    {
+        public MaxBidMinAskRequest(string pairId) : base()
+        {
+            arguments = new Dictionary<string, string>();
+
+            if (pairId != null) arguments.Add("currencyPair", pairId);
+
+            GenerateRequest(exchange, "maxbid_minask");
+        }
+    }
+}
