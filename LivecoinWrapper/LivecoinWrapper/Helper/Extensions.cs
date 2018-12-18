74f79ad0dbd390344c660277e96bb7ac58fea99d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-18 06:01:56 +0200

    Create Extensions.cs

---
+++ LivecoinWrapper/LivecoinWrapper/Helper/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+
+using static System.Web.HttpUtility;
+
+namespace LivecoinWrapper.Helper
+{
+    public static class Extensions
+    {
+        public static string ToKeyValueString(this IDictionary<string, string> dict, bool escape = true) =>
+                      string.Join("&", dict.Select(kvp => string.Format("{0}={1}", kvp.Key, escape ? UrlEncode(kvp.Value) : kvp.Value)));
+    }
+}
