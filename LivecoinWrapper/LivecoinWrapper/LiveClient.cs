d2ab6d05b44fdc45b3316b9bff30d334847f19d6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-20 18:01:25 +0200

    Update LiveClient.cs

--- LivecoinWrapper/LivecoinWrapper/LiveClient.cs
+++ LivecoinWrapper/LivecoinWrapper/LiveClient.cs
@@ -3,12 +3,14 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using LivecoinWrapper.DataLayer.RequestData;
+using LivecoinWrapper.Helper;
+using System.Text;
 
 namespace LivecoinWrapper
 {
     public abstract class LiveClient : IDisposable
     {
-        protected HttpClient httpClient;
+        private readonly HttpClient httpClient;
 
         private const string baseAddress = "https://api.livecoin.net";
 
@@ -17,28 +19,39 @@ namespace LivecoinWrapper
             httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
         }
 
-        public LiveClient(string apiKey, string apiSec)
+        public LiveClient(string apiKey)
         {
-            // todo
+            httpClient = new HttpClient { BaseAddress = new Uri(baseAddress) };
+            httpClient.DefaultRequestHeaders.Add("Api-key", apiKey);
         }
 
         protected async Task<T> HttpGetAsync<T>(RequestObject requestObj)
         {
+            if (requestObj.Sign != null) httpClient.DefaultRequestHeaders.Add("Sign", requestObj.Sign);
+
             var response = await httpClient.GetAsync(requestObj.Url).ConfigureAwait(false);
 
-            response.EnsureSuccessStatusCode();         // throw if web request failed
+            //response.EnsureSuccessStatusCode();         // throw if web request failed
 
             string json = await response.Content.ReadAsStringAsync();
 
             return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
         }
 
-        //protected async Task<T> HttpPostAsync<T>(RequestObject requestObj)
-        //{
-        //    //todo
+        protected async Task<T> HttpPostAsync<T>(RequestObject requestObj)
+        {
+            httpClient.DefaultRequestHeaders.Add("Sign", requestObj.Sign);
 
-        //    return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(json));
-        //}
+            var response = await httpClient.PostAsync(requestObj.Url,
+                new StringContent(requestObj.arguments.ToKeyValueString(),
+                    Encoding.UTF8, "application/x-www-form-urlencoded")).ConfigureAwait(false);
+
+            //response.EnsureSuccessStatusCode();         // throw if web request failed
+
+            string json = await response.Content.ReadAsStringAsync();
+
+            return await Task.Run(() => JsonConvert.DeserializeObject<T>(json));
+        }
 
         public void Dispose() => httpClient.Dispose();
     }
