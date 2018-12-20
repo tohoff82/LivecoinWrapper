c40bcb64ed9f5e7d9e82b26de8bae433ade0fca0
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-20 18:20:48 +0200

    Update LiveClientPrivate.cs
    Method ReturnTradeHistoryAsync  Complete

--- LivecoinWrapper/LivecoinWrapper/LiveClientPrivate.cs
+++ LivecoinWrapper/LivecoinWrapper/LiveClientPrivate.cs
@@ -1,36 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using LivecoinWrapper.DataLayer.ReciveData;
+using LivecoinWrapper.DataLayer.RequestData;
+using LivecoinWrapper.DataLayer;
+using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using LivecoinWrapper.DataLayer.ExceptionData;
 
 namespace LivecoinWrapper
 {
     public class LiveClientPrivate :LiveClient
     {
-        private static LiveClientPrivate instance;
-        private static object syncRoot = new Object();
+        private readonly string apiSec;
 
-        private readonly string publicKey;
-        private readonly string secretKey;
-
-        protected LiveClientPrivate(string publicKey, string secretKey):base()
+        public LiveClientPrivate(string apiKey, string apiSec) : base(apiKey)
         {
-            this.publicKey = publicKey;
-            this.secretKey = secretKey;
+            this.apiSec = apiSec;
         }
 
-        public LiveClientPrivate Set (string pubKiy, string secKey)
-        {
-            if (instance == null)
-            {
-                lock (syncRoot)
-                {
-                    if (instance == null)
-                        instance = new LiveClientPrivate(pubKiy, secKey);
-                }
-            }
-
-            return instance;
-        }
+        /// <summary>
+        /// Get information about his recent transactions, the result can be limited by the relevant parameters
+        /// </summary>
+        /// <param name="pairId">The currency pair identifier. If not specified, all pairs will be returned.</param>
+        /// <param name="orderDesc">The sort order. If true, then new orders will be the first, if false, then old orders will be the first.</param>
+        /// <param name="limit">Number of elements per page</param>
+        /// <param name="offset">Position of the first element on the page in the selection</param>
+        /// <returns>List PrivateTrade</returns>
+        public async Task<List<PrivateTrade>> ReturnTradeHistoryAsync(string pairId = null, bool orderDesc = true, ushort limit = 100, ushort offset = 0) =>
+                await HttpGetAsync<List<PrivateTrade>>(new TradeHistoryRequest(apiSec, pairId, orderDesc, limit, offset));
     }
 }
