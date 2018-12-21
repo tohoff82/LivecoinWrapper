609ed5d5ba2c286c54547f6aac54d0bc71599efe
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-21 06:53:33 +0200

    Create OrdersRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/OrdersRequest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class OrdersRequest : RequestObject
+    {
+        public OrdersRequest(string apiSec,   string pairId, string orderStatus, ulong? issuedFrom,
+                             ulong? issuedTo, uint startRow, uint endRow) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["openClosed"] = orderStatus,
+                ["startRow"]   = startRow.ToString(),
+                ["endRow"]     = endRow.ToString()
+            };
+
+            if (pairId != null)     arguments.Add("currencyPair", pairId);
+            if (issuedFrom != null) arguments.Add("issuedFrom", issuedFrom.ToString());
+            if (issuedTo != null)   arguments.Add("issuedTo", issuedTo.ToString());
+
+            GenerateRequest(exchangeAuthGET, "client_orders");
+        }
+    }
+}
