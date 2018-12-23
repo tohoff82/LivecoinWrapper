f0f109da546938314ba32452fc023836be7fbdfc
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 12:53:52 +0200

    Rename LivecoinApiException -->LiveException

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ExceptionData/LiveException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LivecoinWrapper.DataLayer.ExceptionData
+{
+    class LiveException : Exception
+    {
+        public LiveException()
+        {
+        }
+
+        public LiveException(string message)
+            : base(message)
+        {
+        }
+
+        public LiveException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
