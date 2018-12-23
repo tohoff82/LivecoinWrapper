12890f5a7a277f8d6088ce2ea68c3a7dee84b107
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 18:56:47 +0200

    Create Fee.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Fee.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Fee
+    {
+        [JsonProperty("success")]
+        public bool Success { get; private set; }
+
+        private readonly decimal fee;
+        public decimal CurrentFee { get => fee; }
+
+        [JsonConstructor]
+        public Fee(string fee)
+        {
+            decimal.TryParse(fee, Any, InvariantCulture, out this.fee);
+        }
+    }
+}
