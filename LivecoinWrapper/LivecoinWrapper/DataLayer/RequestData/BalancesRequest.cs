f2a99d6010eba738d9853fa12a89d756cd4e4bae
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 11:55:07 +0200

    Create BalancesRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/BalancesRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class BalancesRequest : RequestObject
+    {
+        public BalancesRequest(string apiSec, string pairIds) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["currency"] = pairIds
+            };
+
+            GenerateRequest(exchangeAuth_GET, "balances");
+        }
+    }
+}
