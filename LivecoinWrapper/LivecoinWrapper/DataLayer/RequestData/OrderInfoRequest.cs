1dfd9df0a9bd9b59c9510af5177c958ad2617a7c
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 10:01:09 +0200

    Create OrderInfoRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/OrderInfoRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class OrderInfoRequest : RequestObject
+    {
+        public OrderInfoRequest(ulong orderId) : base()
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["orderId"] = orderId.ToString()
+            };
+
+            GenerateRequest(exchangeAuth_GET, "order");
+        }
+    }
+}
