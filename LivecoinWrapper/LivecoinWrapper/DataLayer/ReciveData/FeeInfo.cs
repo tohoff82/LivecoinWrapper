f2cc914ba778f09ea382054e4d77a0985f48a75e
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 19:32:32 +0200

    Create FeeInfo.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/FeeInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class FeeInfo
+    {
+        [JsonProperty("success")]
+        public bool Success { get; private set; }
+
+        public Dictionary<string, decimal> Info { get; private set; } = new Dictionary<string, decimal>();
+
+        private readonly decimal fee;
+        private readonly decimal last30DaysAmountAsUSD;
+
+        [JsonConstructor]
+        public FeeInfo(string fee, string commission, string last30DaysAmountAsUSD)
+        {
+            if (fee != null)
+            {
+                decimal.TryParse(fee, Any, InvariantCulture, out this.fee);
+                Info.Add("Fee", this.fee);
+            }
+            else
+            {
+                decimal.TryParse(commission,            Any, InvariantCulture, out this.fee);
+                decimal.TryParse(last30DaysAmountAsUSD, Any, InvariantCulture, out this.last30DaysAmountAsUSD);
+                Info.Add("Fee", this.fee);
+                Info.Add("MonthlyUSDVolume", this.last30DaysAmountAsUSD);
+            }
+        }
+    }
+}
