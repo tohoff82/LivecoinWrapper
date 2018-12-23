ae094fa04905fc22582f45fc77cd2ca45ffa1df4
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 18:56:42 +0200

    Create FeeRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/FeeRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class FeeRequest : RequestObject
+    {
+        public FeeRequest(string apiSec) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>();
+
+            GenerateRequest(exchangeAuth_GET, "commission");
+        }
+    }
+}
