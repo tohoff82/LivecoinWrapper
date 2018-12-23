117f6ad5a99d03698bf447dfd53b9bfc52ca693f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 12:24:01 +0200

    Create CurrencyId.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/CurrencyId.cs
@@ -0,0 +1,12 @@
+﻿namespace LivecoinWrapper.DataLayer
+{
+    public static class CurrencyId
+    {
+        public const string usd = "USD";
+        public const string btc = "BTC";
+        public const string eth = "ETH";
+        public const string ltc = "LTC";
+        public const string emc = "EMC";
+        public const string wgr = "WGR";
+    }
+}
