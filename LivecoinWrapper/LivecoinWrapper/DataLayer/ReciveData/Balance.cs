06e4c0f3f286407aa8ce838c19c56bf151b47c51
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 12:15:14 +0200

    Create Balance.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Balance.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+using static LivecoinWrapper.Helper.Enums;
+using static System.Globalization.CultureInfo;
+using static System.Globalization.NumberStyles;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Balance
+    {
+        [JsonProperty("type")]
+        public BalanceType BalanceType { get; private set; }
+
+        [JsonProperty("currency")]
+        public string CurrencyId { get; private set; }
+
+        private readonly decimal value;
+        public decimal Value { get => value; }
+
+        [JsonConstructor]
+        public Balance(string value)
+        {
+            decimal.TryParse(value, Any, InvariantCulture, out this.value);
+        }
+    }
+}
