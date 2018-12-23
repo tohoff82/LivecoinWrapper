c95d6b0f05ea1070b5bbc9660429f3c4d8f722b1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-23 18:39:33 +0200

    Create TransactionsRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/TransactionsRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class TransactionsRequest : RequestObject
+    {
+        public TransactionsRequest(string apiSec, ulong startTime, ulong endTime, string transType, ushort resLimit, ushort offset):base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["start"]  = startTime.ToString(),
+                ["end"]    = endTime.ToString(),
+                ["limit"]  = resLimit.ToString(),
+                ["offset"] = offset.ToString()
+            };
+
+            if (transType != "all") arguments.Add("types", transType);
+
+            GenerateRequest(payment_GET, "history/transactions");
+        }
+    }
+}
