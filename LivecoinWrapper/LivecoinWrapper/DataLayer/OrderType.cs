8e41273dc482777af4542ceff5bdaeadd3e9b221
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-12-25 15:11:59 +0200

    Update OrderType.cs

--- LivecoinWrapper/LivecoinWrapper/DataLayer/OrderType.cs
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/OrderType.cs
@@ -6,8 +6,12 @@ namespace LivecoinWrapper.DataLayer
 {
     public static class OrderType
     {
-        public const string _defoult = "false";
-        public const string _buy     = "BUY";
-        public const string _sell    = "SELL";
+        public const string _defoult     = "false";
+        public const string _buy         = "BUY";
+        public const string _sell        = "SELL";
+        public const string _limit_buy   = "LIMIT_BUY";
+        public const string _limit_sell  = "LIMIT_SELL";
+        public const string _market_buy  = "MARKET_BUY";
+        public const string _market_sell = "MARKET_SELL";
     }
 }
