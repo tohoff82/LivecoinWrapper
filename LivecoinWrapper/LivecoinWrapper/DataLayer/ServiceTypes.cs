785777a423b42d0ca9e83c6a7f74c87ec823f7fe
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-05 20:01:08 +0200

    Create ServiceTypes.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ServiceTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace LivecoinWrapper.DataLayer
+{
+    public static class ServiceTypes
+    {
+        //----- Default Types ---------------------//
+        public const string _false       = "false";
+        public const string _all         = "ALL";
+
+        //------ Order Types ----------------------//
+        public const string _limit_buy   = "LIMIT_BUY";
+        public const string _limit_sell  = "LIMIT_SELL";
+        public const string _market_buy  = "MARKET_BUY";
+        public const string _market_sell = "MARKET_SELL";
+
+        //------ Order & Transaction Types -------//
+        public const string _buy         = "BUY";
+        public const string _sell        = "SELL";
+
+        //------ Transaction Types --------------//
+        public const string _deposit     = "DEPOSIT";
+        public const string _withdrawal  = "WITHDRAWAL";
+
+        //------ Balance Types -----------------//
+        public const string _total      = "total";                             //total balance
+        public const string _trade      = "trade";                             //funds available for trading
+        public const string _available  = "available";                         //funds in open orders
+        public const string _available_withdrawal = "available_withdrawal";    //funds available for withdrawal
+    }
+}
