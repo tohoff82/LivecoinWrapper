84524808eb0321bd93643379aee2f8f8a4a92233
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-06 09:54:30 +0200

    Create CancelRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/CancelRequest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class CancelRequest : RequestObject
+    {
+        public CancelRequest(string apiSec, string pairId, ulong orderId) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["currencyPair"] = pairId,
+                ["orderId"] = orderId.ToString()
+            };
+
+            GenerateRequest(exchangeAuth_GET, "cancellimit");
+        }
+    }
+}
