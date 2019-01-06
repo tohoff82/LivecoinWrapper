88f5001a87a0b4bbc0f7406e3e177863197e277f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-06 17:40:13 +0200

    Create Wallet.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/ReciveData/Wallet.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+using static System.StringSplitOptions;
+
+namespace LivecoinWrapper.DataLayer.ReciveData
+{
+    public class Wallet
+    {
+        [JsonProperty("fault")]
+        public string Fault { get; private set; }
+
+        [JsonProperty("userId")]
+        public uint UserId { get; private set; }
+
+        [JsonProperty("userName")]
+        public string UserName { get; private set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; private set; }
+
+        public string Address { get; private set; }
+        public string Memo { get; private set; }
+
+        [JsonConstructor]
+        public Wallet(string wallet)
+        {
+            string[] spl = wallet.Split(new string[] { "::" }, RemoveEmptyEntries);
+            if (spl.Length != 0 && spl.Length == 2) { Address = spl[0]; Memo = spl[1]; }
+            else Address = wallet;
+        }
+    }
+}
