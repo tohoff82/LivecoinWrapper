a989f23ac72b3a1ec00765554705f40f1f2878bf
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-06 17:37:51 +0200

    Create WalletAddressRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/WalletAddressRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class WalletAddressRequest : RequestObject
+    {
+        public WalletAddressRequest(string apiSec, string currencyId) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["currency"] = currencyId
+            };
+
+            GenerateRequest(payment_GET, "get/address");
+        }
+    }
+}
