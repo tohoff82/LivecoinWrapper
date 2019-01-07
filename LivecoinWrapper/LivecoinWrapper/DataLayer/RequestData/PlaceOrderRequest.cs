33391416305a8cf4f61f8d489dde435e83ae5000
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 13:12:39 +0200

    Update PlaceOrderRequest.cs

--- LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/PlaceOrderRequest.cs
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/PlaceOrderRequest.cs
@@ -7,12 +7,12 @@ namespace LivecoinWrapper.DataLayer.RequestData
 {
     public class PlaceOrderRequest : RequestObject
     {
-        public PlaceOrderRequest(string apiSec, string type, string pairId, decimal quantity, decimal? price) : base(apiSec)
+        public PlaceOrderRequest(string apiSec, string type, string pairId, decimal quantity, decimal price) : base(apiSec)
         {
             arguments = new SortedDictionary<string, string>
             {
                 ["currencyPair"] = pairId,
-                ["quantity"]     = quantity.ToString()
+                ["quantity"]     = quantity.ToString(culture)
             };
 
             if (type == _limit_buy)  Generate(price, "buylimit");
@@ -22,9 +22,9 @@ namespace LivecoinWrapper.DataLayer.RequestData
             if (type == _market_sell) Generate(price, "sellmarket");
         }
 
-        private void Generate(decimal? price, string apiMethod)
+        private void Generate(decimal price, string apiMethod)
         {
-            if(price != null) arguments.Add("price", price.ToString());
+            if(price != _d_null) arguments.Add("price", price.ToString(culture));
             GenerateRequest(exchangeAuth_POST, apiMethod);
         }
     }
