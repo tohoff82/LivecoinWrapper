84edfcfbb77c9cfcd4ea307b15a305bb42a43686
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-01-07 09:40:44 +0200

    Create WithdrawRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/WithdrawRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class WithdrawRequest : RequestObject
+    {
+        public WithdrawRequest(string apiSec, string currencyId, decimal amount, string address, string memo) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["currency"] = currencyId,
+                ["amount"]   = amount.ToString(),
+                ["wallet"]   = memo == null ? address : new StringBuilder(address).AppendFormat("{0}::{1}", address, memo).ToString()
+            };
+
+            GenerateRequest(payment_POST, "out/coin");
+        }
+    }
+}
