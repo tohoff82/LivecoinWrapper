162b434c1560843273344c607e092c78e246f6a2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-04 07:43:31 +0300

    Create VoucherMakeRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/VoucherMakeRequest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class VoucherMakeRequest : RequestObject
+    {
+        public VoucherMakeRequest(string apiSec, decimal amount, string currId, string description = null, string forUser = null) : base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["amount"] = amount.ToString(culture),
+                ["currency"] = currId,
+                ["description"] = description
+            };
+
+            if (forUser != null) arguments["forUser"] = forUser;
+
+            GenerateRequest(payment_POST, "voucher/make");
+        }
+    }
+}
