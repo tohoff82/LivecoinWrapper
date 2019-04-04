ed6b76693e1ea52ed2453c73b1f83dbabb4374a2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-04 09:26:39 +0300

    Create VoucherAmountRequest.cs

---
+++ LivecoinWrapper/LivecoinWrapper/DataLayer/RequestData/VoucherAmountRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using static LivecoinWrapper.Helper.Enums.RequestType;
+
+namespace LivecoinWrapper.DataLayer.RequestData
+{
+    public class VoucherAmountRequest : RequestObject
+    {
+        public VoucherAmountRequest(string apiSec, string voucherCode):base(apiSec)
+        {
+            arguments = new SortedDictionary<string, string>
+            {
+                ["voucher_code"] = voucherCode
+            };
+
+            GenerateRequest(payment_POST, "voucher/amount");
+        }
+    }
+}
