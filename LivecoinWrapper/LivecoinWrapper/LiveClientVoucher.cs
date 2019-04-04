7f7217f25393d0d43cb41023dc756e414a59931f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-04-04 07:23:09 +0300

    Create LiveClientVoucher.cs

---
+++ LivecoinWrapper/LivecoinWrapper/LiveClientVoucher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LivecoinWrapper
+{
+    class LiveClientVoucher : LiveClient
+    {
+        private readonly string apiSec;
+
+        public LiveClientVoucher(string apiKey, string apiSec) : base(apiKey)
+        {
+            this.apiSec = apiSec;
+        }
+    }
+}
